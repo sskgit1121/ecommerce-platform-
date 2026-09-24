5175e239ef4df79c0e785ec315b9d1d977ff04d3
CE00120220 <goldy.sarraf@mapmyindia.com>
2026-09-24 11:46:13 +0530

    feat: Symmetric token verification project baseline architecture backup

--- order-service/src/main/java/com/ssk/order/client/ProductResponse.java
+++
@@ -1,5 +0,0 @@
-package com.ssk.order.client;
-
-import java.math.BigDecimal;
-
-public record ProductResponse(String id, String name, BigDecimal price, String description) {}
--- product-service/src/main/java/com/ssk/product/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssk.product.repository;
-
-
-import com.ssk.product.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {}
-
